a1a734c8088719068e98f45d0bcdc1f61766258b
Graham Siener <gsiener@pivotallabs.com>
2012-12-18 11:03:35 -0500

    More rules, and move last accessed to be greater than vs. exact

--- rules.rb
+++ rules.rb
@@ -12,22 +12,34 @@ Maid.rules do
     trash(dir('~/Downloads/*.iso'))
   end
 
-  rule 'Delete Mac OS X applications in disk images' do
-    trash(dir('~/Downloads/*.dmg'))
+  rule 'Trash downloaded software' do
+    # These can generally be downloaded again very easily if needed...
+    # but just in case, give me a few days before trashing them.
+    dir('~/Downloads/*.{dmg,pkg}').each do |p|
+      trash(p) if accessed_at(p) > 3.days.ago
+    end
+
+    osx_app_extensions = %w(app dmg pkg wdgt)
+    osx_app_patterns = osx_app_extensions.map { |ext| (/\.#{ext}\/$/) }
+
+    zips_with_osx_apps_inside = dir('~/Downloads/*.zip').select do |path|
+      candidates = zipfile_contents(path)
+      candidates.any? { |c| osx_app_patterns.any? { |re| c.match(re) } }
+    end
+
+    trash(zips_with_osx_apps_inside)
   end
 
-  rule 'Delete Mac OS X applications in zip files' do
-    found = dir('~/Downloads/*.zip').select { |path|
-      zipfile_contents(path).any? { |c| c.match(/\.app$/) }
-    }
-
-    trash(found)
+  rule 'Move Mac OSX applications to Applications' do
+    dir('~/Downloads/*.app').each do |path|
+      move(path, '/Applications/')
+    end
   end
 
   rule 'Delete Misc Screenshots' do
-    dir('~/Desktop/Screen Shot *').each do |path|
-      if 3.days.since?(accessed_at(path))
-        trash(path)
+    dir('~/Desktop/Screen Shot *').each do |p|
+      if accessed_at(p) > 3.days.ago
+        trash(p)
       end
     end
   end
@@ -41,17 +53,28 @@ Maid.rules do
   end
   
   rule 'Move Videos to Movies folder' do
-    dir('~/Downloads/*.mp4').each do |path|
+    dir('~/Downloads/*.{mp4,mpg}').each do |path|
       move(path, '~/Desktop/Movies/')
     end
   end
   
-  rule 'Move Videos to Movies folder' do
-    dir('~/Downloads/*.mpg').each do |path|
-      move(path, '~/Desktop/Movies/')
+  rule "Trash files that shouldn't have been downloaded" do
+    # It's rare that I download these file types and don't put them somewhere else quickly.
+    # More often, these are still in Downloads because it was an accident.
+    dir('~/Downloads/*.{csv,doc,docx,ics,ppt,pptx,js,rb,xml,xlsx}').each do |p|
+      trash(p) if accessed_at(p) > 3.days.ago
     end
+
+    # Quick 'n' dirty duplicate download detection
+    trash(dir('~/Downloads/* (1).*'))
+    trash(dir('~/Downloads/* (2).*'))
+    trash(dir('~/Downloads/*.1'))
   end
   
+  # rule "Bookmark links on Desktop" do
+  # 
+  #   end
+
   # # NOTE: Currently, only Mac OS X supports `downloaded_from`.
   # rule 'Old files downloaded while developing/testing' do
   #   dir('~/Downloads/*').each do |path|
